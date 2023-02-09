ea02df5ea06ff29794922ef7f826b5984c52c364
KO Myung-Hun <komh@chollian.net>
2023-02-09 15:41:46 +0900

    my: configure.cmd: update
    1. enable high-mem support
    2. prepend /@unixroot to a prefix
    3. return an exit code correctly
    
        modified:   configure.cmd

--- configure.cmd
+++ configure.cmd
@@ -5,5 +5,15 @@ d=$(dirname "$0" | tr '\\' /)
 n=configure
 test -f "$d/$n." || { echo "\`$d/$n' not found !!!"; exit 1; }
 
-opts="--enable-shared --enable-static --enable-nls --enable-extra-encodings"
+export LDFLAGS=-Zhigh-mem
+
+opts="
+    --prefix=/@unixroot/usr/local
+    --enable-shared
+    --enable-static
+    --enable-nls
+    --enable-extra-encodings
+"
+
 "$d/$n" $opts "$@" 2>&1 | tee "$n.log"
+exit ${PIPESTATUS[0]}
